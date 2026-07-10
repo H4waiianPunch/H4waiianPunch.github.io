cbae63bd9ececbf863cca8bf6d0ab904b5a881ce
H4waiianPunch <118198739+H4waiianPunch@users.noreply.github.com>
2026-07-10 19:49:06 -0300

    Update bingo-data.js

--- bingo-data.js
+++ bingo-data.js
@@ -12,11 +12,11 @@ export const bingoData = {
       tiles: [
         {
           title: "God Wars Drop",
-          image: "images/tile-1.svg",
+          image: "https://i.redd.it/gt0fy0wr1jz91.png",
           description: "Earn an approved God Wars Dungeon unique for your team.",
           requirements: "Submit a screenshot showing the drop, player name, and game message.",
-          completed: false,
-          completedBy: ""
+          completed: true,
+          completedBy: "The Ryanster"
         },
         {
           title: "Barrows Unique",
